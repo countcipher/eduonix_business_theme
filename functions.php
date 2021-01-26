a3b6ba6dd3b98f497c410183f8e6fc561fab49b4
Count Cipher <chrisharlow366@yahoo.com>
2021-01-26 12:42:55 -0500

    com

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once('wp_bootstrap_navwalker.php');
+
+//THEME SUPPORT
+//==============================
+function business_theme_setup(){
+
+    //REGISTER MENU
+    //==============================
+    register_nav_menus(array(
+        'primary'   =>  'Primary Menu'
+    ));
+
+    //LOGO SUPPORT
+    //===============================
+    add_theme_support('custom-logo');
+}
+
+//FIRES THEME SUPPORT FUNCTIONS
+//======================================
+add_action('after_setup_theme', 'business_theme_setup');
